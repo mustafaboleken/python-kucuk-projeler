a2c807eb6404a1829829d2da22858f3ecce9632f
MustafaB <GuardianOfAsgaard1@users.noreply.github.com>
2017-01-21 12:15:35 +0200

    Update yerine_koyma_şifrelemesi.py

--- Proje-2-Yerine-Koyma-Sifrelemesi/yerine_koyma_şifrelemesi.py
+++ Proje-2-Yerine-Koyma-Sifrelemesi/yerine_koyma_şifrelemesi.py
@@ -2,19 +2,17 @@ key='ABCÇDEFGĞHIJKLMNOÖPRSŞTUÜVYZQWX.@,-_! 0123456789'
 
 keyrec='4ÇQÜMGK6NJ2C5SP0ZW3E7HVOY1ÖĞI8?-,.@=+!FTADULŞRXB'
 
-print('1: Veriyi Şifreleme')
-
-print('2: Şifreyi Çözme')
+print('1: Veriyi Şifreleme\n2: Şifreyi Çözme')
 
 e=input('Hangi işlemi yapmak istiyorsunuz: ')
 
-if int(e) in [1]:
+if e == '1':
 	encryption=input('Şifrelenecek veriyi giriniz: ')
 	f=str.maketrans(key,keyrec)
 	encryption=encryption.upper().translate(f)
 	print(encryption)
 
-elif int(e) in [2]:
+elif e == '2':
 	decryption=input('Şifreli veriyi giriniz: ')
 	p=str.maketrans(keyrec,key)
 	decryption=decryption.upper().translate(p)
